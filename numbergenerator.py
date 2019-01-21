7d17468ac918139c5d0ef8fce8dd0d35fdfe8eea
Kara Watts <kwatts2017@my.fit.edu>
2019-01-20 19:05:22 -0500

    random number generator

---
+++ numbergenerator.py
@@ -0,0 +1,29 @@
+# Town List
+
+import random    
+town_list=[]    
+for i in range (1):    
+    town_list.append(random.randrange(1,27,1))    
+
+
+
+
+# house samples
+
+import random
+
+house_list = random.sample(range(1, 706), 1)
+
+print(house_list)
+
+
+
+# Subject Select
+
+import random    
+subject_list=[]    
+for i in range (1):    
+    subject_list.append(random.randrange(1,3))    
+
+print (subject_list)                    
+
