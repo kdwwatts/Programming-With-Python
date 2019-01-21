37fb77d4813d30eac7d83d584e5a979c60986ac6
Kara Watts <kwatts2017@my.fit.edu>
2019-01-20 20:00:41 -0500

    Celsius to Fahrenheit Converter

---
+++ CtoFConverter.py
@@ -0,0 +1,8 @@
+# Program to convert celsius to fahrenheit
+
+#change this value for a different result
+celsius = 27
+
+#calculate fahrenheit
+fahrenheit = (celsius *1.8)+32
+print('%0.1f degree Celsius is equal to %0.1f degree fahrenheit' %(celsius,fahrenheit))
